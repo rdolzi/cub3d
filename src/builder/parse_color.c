931ca7edca4bc468e7f6fa29948d1f62ca6d4512
roger-rab-bit <ruggero-dolzi@virgilio.it>
2024-04-16 03:59:16 +0200

    wip parse_color

--- src/builder/parse_color.c
+++ src/builder/parse_color.c
@@ -6,11 +6,23 @@
 /*   By: rdolzi <rdolzi@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 02:11:15 by rdolzi            #+#    #+#             */
-/*   Updated: 2024/04/16 02:38:16 by rdolzi           ###   ########.fr       */
+/*   Updated: 2024/04/16 03:58:36 by rdolzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cube.h"
+
+void convert_in_hexadecimal(t_game *game, int color)
+{
+    int *rgb;
+    // fa lo split del path, controlla che siano 3 int(atoi) e li setta in array
+    rgb = get_rgb(game, color);
+    // controlla che siano rgb validi ovvero da 0 a 255
+    if (is_valid_rgb(rgb))
+        convert_rgb_to_hex(game, color, rgb); // converte rgb in hexadecimal
+    else
+        clean_exit(game, throw_exception(MAP_EXCEPTION, ERR_MAP_COLOR, NULL));
+}
 
 //parse_colors
 //  check rgb
@@ -18,9 +30,18 @@
 //  check color validity
 void parse_color(t_game *game)
 {
-    (void) game;
-    printf("\nin parse_color\n");
-    printf("game->type[FLOOR].path:%s| len:%d\n", game->type[FLOOR].path, (int)ft_strlen(game->type[FLOOR].path));
+    enum e_color color;
+    
+	printf("entra in parse_color\n");
+    color = FLOOR;
+    while (color <= FLOOR) {
+        if (game->type[color].path == NULL)
+            clean_exit(game, throw_exception(MAP_EXCEPTION, ERR_MAP_COLOR, NULL));
+        printf("game->type[color].path:%s| len:%d\n", game->type[color].path, (int)ft_strlen(game->type[color].path));
+        convert_in_hexadecimal(game, (int)color);
+        color++;
+    }
+	printf("esce in parse_color\n");
 }
 
 
